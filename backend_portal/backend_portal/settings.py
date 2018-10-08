29f1651861c6e9c3cd6a49aafd6e6b89921912c9
vinibfranc <vinibfranc@gmail.com>
2018-10-08 18:00:46 -0300

    Conexão com o MySQL

--- backend_portal/backend_portal/settings.py
+++ backend_portal/backend_portal/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'backend_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
 
